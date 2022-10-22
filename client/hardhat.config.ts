8cb72e181f7d5f4b8b8aa7d613d0e3b9407d0a69
Nguyễn Trần Hương <74103610+tranhuong2302@users.noreply.github.com>
2022-10-22 12:21:39 +0700

    NFTMarket

---
+++ client/hardhat.config.ts
@@ -0,0 +1,18 @@
+import "@nomiclabs/hardhat-waffle";
+import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
+
+const GOERLI_URL = process.env.GOERLI_URL as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+console.log(GOERLI_URL);
+const config: HardhatUserConfig = {
+  solidity: "0.8.11",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [ PRIVATE_KEY ],
+    }
+  },
+};
+
+export default config;
